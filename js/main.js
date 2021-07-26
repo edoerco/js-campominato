b76fad84ca57dc2410d2a9ee262d781a581bb861
Edoardo <ercolino.edoardo@gmail.com>
2021-07-26 16:24:23 +0200

    Conclusione esercizio

--- js/main.js
+++ js/main.js
@@ -1,21 +1,27 @@
 // Il computer deve generare 16 numeri casuali (le nostre bombe) tra 1 e 100.
 var nBomba = [];
-while(nBomba.length < 16){  //modificare il numero di 'bombe'
-    var random = Math.floor(Math.random() * 100) + 1;
+var maxNumber = 100;
+var bombs = 16;
+while(nBomba.length < bombs){  //modificare il numero di 'bombe'
+    var random = Math.floor(Math.random() * maxNumber) + 1;
     // I numeri non possono essere duplicati.
-    if(nBomba.indexOf(random) === -1) nBomba.push(random); 
+    if(nBomba.indexOf(random) === -1) {
+        nBomba.push(random); 
+    } 
 }
 console.log(nBomba);
 
 // In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
 var nUtente = [];
-var items = 10;
+var games = 84;
 
-while(nUtente.length < items) {
+while(nUtente.length < games) {
     var scelte = parseInt(prompt('Inserisci un numero da 1 a 100'));
 
     // L’utente non può inserire più volte lo stesso numero.
-    if((nUtente.indexOf(scelte) === -1) && (isNaN(scelte) == false) && (scelte >= 1 && scelte <= 100)) nUtente.push(scelte); 
+    if((nUtente.indexOf(scelte) === -1) && (isNaN(scelte) == false) && (scelte >= 1 && scelte <= maxNumber) && (nBomba.includes(scelte) == false)) {
+        nUtente.push(scelte); 
+    } 
     
     // Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
     // La partita termina quando il giocatore inserisce un numero “vietato”  
@@ -24,22 +30,12 @@ while(nUtente.length < items) {
         break;
     }
     // o raggiunge il numero massimo possibile di numeri consentiti.
-    if(nUtente.length == items) {
+    if(nUtente.length == games) {
         alert('Hai vinto');
     }
 }
 console.log(nUtente)
 
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
-
-
-
-
-
-
-
-
-
-
-
-// funzioni
+var punteggio = nUtente.length;
+console.log('il punteggio è ' + punteggio);
